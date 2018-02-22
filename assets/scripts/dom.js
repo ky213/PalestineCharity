eb152d2405dbd5735f400ebe663c290d55126edc
ky213 <y.keddaoui@gmail.com>
2018-02-22 14:34:59 +0100

    main menu first prototype

--- assets/scripts/dom.js
+++
@@ -1,9 +0,0 @@
-export function offset(el) {
-   var rect = el.getBoundingClientRect(),
-      scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
-      scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-   return {
-      top: rect.top + scrollTop,
-      left: rect.left + scrollLeft
-   }
-}
