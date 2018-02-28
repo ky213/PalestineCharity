1fe23c93eceb8f763814c623e9cfd5375de42814
ky213 <y.keddaoui@gmail.com>
2018-02-28 09:09:56 +0100

    Hide menu on scroll

--- script.js
+++ script.js
@@ -22,4 +22,16 @@ controls.addEventListener("click", function (event) {
         slides[1].style.left = slideLeft + 100 + "%";
         slides[2].style.left = slideLeft + 200 + "%";
     }
+});
+
+/* Menu hide on scroll */
+var menu = document.querySelector("#menu");
+var lastScroll = 0;
+document.addEventListener('scroll', function (event) {
+    if (event.pageY - lastScroll > 0 && event.pageY > 135)
+        menu.style.top = "-135px";
+    else if (event.pageY - lastScroll < -10)
+        menu.style.top = "0";
+
+    lastScroll = event.pageY;
 })
