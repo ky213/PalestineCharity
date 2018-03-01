233801b8675fcf16ea8ff867fca95b6594f8e60f
ky213 <y.keddaoui@gmail.com>
2018-03-01 19:26:23 +0100

    facts section counters

--- script.js
+++ script.js
@@ -30,8 +30,60 @@ var lastScroll = 0;
 document.addEventListener('scroll', function (event) {
     if (event.pageY - lastScroll > 0 && event.pageY > 135)
         menu.style.top = "-135px";
-    else if (event.pageY - lastScroll < -10)
+    else if (event.pageY - lastScroll < -10 || event.pageY < 135)
         menu.style.top = "0";
 
     lastScroll = event.pageY;
 })
+
+/* Facts counters */
+var numbers = document.querySelectorAll('#facts h1');
+//counts top distances
+var h1 = numbers[0].offsetTop + numbers[0].clientHeight;
+var h2 = numbers[1].offsetTop + numbers[1].clientHeight;
+var h3 = numbers[2].offsetTop + numbers[2].clientHeight;
+var h4 = numbers[3].offsetTop + numbers[3].clientHeight;
+
+function count(el, max) {
+    var i = 0;
+    var int = setInterval(function () {
+        el.innerHTML = i.toLocaleString('en');
+        if (i >= max) clearInterval(int);
+        i += Math.floor(max / 50);
+    }, 30)
+}
+
+//count roll up
+var count1 = false;
+var count2 = false;
+var count3 = false;
+var count4 = false;
+function counters(event) {
+    var scroll = event.pageY;
+    var view = window.innerHeight;
+
+    if (scroll >= h1 - view && !count1) {
+        count(numbers[0], 78250);
+        count1 = true;
+    }
+
+    if (scroll >= h2 - view && !count2) {
+        count(numbers[1], 34314);
+        count2 = true;
+    }
+
+    if (scroll >= h3 - view && !count3) {
+        count(numbers[2], 25458);
+        count3 = true;
+    }
+
+    if (scroll >= h4 - view && !count4) {
+        count(numbers[3], 103570);
+        count4 = true;
+    }
+
+    if (count1 && count2 && count3 && count4)
+        document.removeEventListener('scroll', counters);
+}
+
+document.addEventListener('scroll', counters);
